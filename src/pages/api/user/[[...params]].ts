a60d70e3745ab4ed8738f62c8243e41dac67436e
Weslley <weslleysouza22@gmail.com>
2023-02-02 00:50:20 -0300

    ✨Rota de listar usuários.

--- src/pages/api/user/[[...params]].ts
+++ src/pages/api/user/[[...params]].ts
@@ -4,6 +4,9 @@ import * as Next from "next";
 import {
     Body,
     createHandler,
+    Delete,
+    Get,
+    Param,
     Post,
     Res,
     UnauthorizedException,
@@ -18,6 +21,7 @@ class UserHandler {
         @Body(ValidationPipe) body: CreateUserDTO,
         @Res() res: Next.NextApiResponse
     ) {
+        console.log(123);
         const { cpf, email, name, password } = body;
         try {
             await prismaClient.user.create({
@@ -38,6 +42,17 @@ class UserHandler {
             } else {
                 return res.status(400).json(error);
             }
+        }
+    }
+
+    @Get()
+    public async listUser(@Res() res: Next.NextApiResponse) {
+        try {
+            const users = await prismaClient.user.findMany()
+            console.log(users);
+            return res.status(200).json(users);
+        } catch (error) {
+            return res.status(400).json(error);
         }
     }
 }
