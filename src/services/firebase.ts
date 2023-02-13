91be3a49aa5ebb96e16030f74ebd5bb8fb028a63
Kauê Ronald <kaueronald21.kr@gmail.com>
2023-02-13 10:38:31 -0300

    refactor: atualizando a identação

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,15 +3,15 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCeehfyd99fwQ84uPqtjuA95U-ZfWPKBcs",
-    authDomain: "guru-delivery-98cf0.firebaseapp.com",
-    projectId: "guru-delivery-98cf0",
-    storageBucket: "guru-delivery-98cf0.appspot.com",
-    messagingSenderId: "943524734577",
-    appId: "1:943524734577:web:2420ae2f6230cff6e75dd0"
-  };
-  
+  apiKey: "AIzaSyCeehfyd99fwQ84uPqtjuA95U-ZfWPKBcs",
+  authDomain: "guru-delivery-98cf0.firebaseapp.com",
+  projectId: "guru-delivery-98cf0",
+  storageBucket: "guru-delivery-98cf0.appspot.com",
+  messagingSenderId: "943524734577",
+  appId: "1:943524734577:web:2420ae2f6230cff6e75dd0"
+};
+
 const firestore = getFirestore(initializeApp(firebaseConfig));
 const storage = getStorage(initializeApp(firebaseConfig));
 
-export { firestore ,storage};
+export { firestore, storage };
